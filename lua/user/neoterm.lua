3beb70549333c4b54294a9b4da02e734f8773b5f
Dylan Fierro <fierrodylan34@gmail.com>
2022-08-26 14:20:58 -0600

    change float mode to vertical

--- lua/user/neoterm.lua
+++ lua/user/neoterm.lua
@@ -7,17 +7,17 @@ require("toggleterm").setup{
   insert_mappings = true, -- whether or not the open mapping applies in insert mode
   terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
   persist_size = true,
-  direction = 'float',
+  persist_mode = true,
+  direction = 'vertical',
   close_on_exit = true, -- close the terminal window when the process exits
-  shell = vim.o.shell, -- change the default shell
-  float_opts = {
-    border = 'curved',
-    width = 100,
-    height = 30,
-    winblend = 3,
-    highlights = {
-      border = "Normal",
-      background = "Normal",
-    }
-  }
+  shell = "pwsh.exe", -- change the default shell
+  size = 65,
+  -- float_opts = {
+  --   border = 'curved',
+  --   winblend = 3,
+  --   highlights = {
+  --     border = "Normal",
+  --     background = "Normal",
+  --   }
+  -- }
 }
