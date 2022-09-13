09ea0055d782b4cd121656cab7c7e453e0717d58
Dylan Fierro <fierrodylan34@gmail.com>
2022-09-13 12:22:30 -0600

    packer config update, change path compiled file

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -6,160 +6,169 @@ vim.cmd([[
   augroup end
 ]])
 
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
-end
+local packer = require("packer")
 
-packer.init({
-  display = {
-    open_fn = require('packer.util').float
-  },
-})
-
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'lewis6991/impatient.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use { 'fgheng/winbar.nvim',
-    config = function()
-      require("core.plug.winbar")
-    end
-  }
-  use { 'lukas-reineke/indent-blankline.nvim',
-    config = function()
-      require('core.plug.lines')
-    end
-  }
-  use {
-    'stevearc/aerial.nvim',
-    config = function()
-      require("core.plug.aerial")
-    end
-  }
-  use '~/Documents/Projects/prueba.nvim'
-  -- use 'dylanfierro/core.nvim'
-  -- use 'dylanfierro/live-server.nvim'
-  use { 'andweeb/presence.nvim',
-    config = function()
-      require('core.plug.presence')
-    end
-  }
-  use 'tpope/vim-commentary'
-  use 'rcarriga/nvim-notify'
-  use { 'akinsho/toggleterm.nvim',
-    config = function()
-      require("core.plug.terminal")
-    end
-  }
-  -- File Search
-  use { 'nvim-telescope/telescope.nvim',
-    requires = {
-      'nvim-lua/popup.nvim'
-    },
-    config = function()
-      require('core.plug.telescope')
-    end
-  }
-  -- Themes
-  use 'projekt0n/github-nvim-theme'
-  use { 'Everblush/everblush.nvim', as = 'everblush' }
-  use 'rakr/vim-one'
-  use 'sainnhe/everforest'
-  use 'sam4llis/nvim-tundra'
-  use { 'anuvyklack/pretty-fold.nvim',
-    config = function()
-      require('core.plug.fold')
-    end,
-    requires = {
-      'anuvyklack/fold-preview.nvim',
-      'anuvyklack/keymap-amend.nvim'
+  packer.startup {
+  function(use)
+    use 'wbthomason/packer.nvim'
+    use 'lewis6991/impatient.nvim'
+    use 'nvim-lua/plenary.nvim'
+    use { 'SmiteshP/nvim-gps',
+      config = function()
+        require "core.plug.gps"
+      end
     }
-  }
-  use { 'norcalli/nvim-colorizer.lua',
-    config = function()
-      require('core.plug.colorizer')
-    end
-  }
-  -- Preview
-  use({ "iamcco/markdown-preview.nvim",
-    run = "cd app && npm install",
-    setup = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
-  })
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = {
-      'kyazdani42/nvim-web-devicons',
-    },
-    tag = 'nightly',
-    config = function()
-      require('core.plug.nvim-tree')
-    end,
-  }
-  -- GitHub
-  use { 'lewis6991/gitsigns.nvim',
-    config = function()
-      require('core.plug.gitsigns')
-    end
-  }
-  -- Status Line and Tabs
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = {
-      { 'kyazdani42/nvim-web-devicons', opt = true },
-      { 'romgrk/barbar.nvim',
-        config = function()
-          require("core.plug.barbar")
-        end,
+    use { 'SmiteshP/nvim-navic',
+      config = function()
+        require "core.plug.navic"
+      end
+    }
+    -- use { 'fgheng/winbar.nvim',
+    --   config = function()
+    --     require("core.plug.winbar")
+    --   end
+    -- }
+    use { 'lukas-reineke/indent-blankline.nvim',
+      config = function()
+        require('core.plug.lines')
+      end
+    }
+    use {
+      'stevearc/aerial.nvim',
+      config = function()
+        require("core.plug.aerial")
+      end
+    }
+    use '~/Documents/Projects/prueba.nvim'
+    -- use 'dylanfierro/core.nvim'
+    -- use 'dylanfierro/live-server.nvim'
+    use { 'andweeb/presence.nvim',
+      config = function()
+        require('core.plug.presence')
+      end
+    }
+    use 'tpope/vim-commentary'
+    use 'rcarriga/nvim-notify'
+    use { 'akinsho/toggleterm.nvim',
+      config = function()
+        require("core.plug.terminal")
+      end
+    }
+    -- File Search
+    use { 'nvim-telescope/telescope.nvim',
+      requires = {
+        'nvim-lua/popup.nvim'
       },
+      config = function()
+        require('core.plug.telescope')
+      end
+    }
+    -- Themes
+    use 'projekt0n/github-nvim-theme'
+    use { 'Everblush/everblush.nvim', as = 'everblush' }
+    use 'rakr/vim-one'
+    use 'sainnhe/everforest'
+    use 'sam4llis/nvim-tundra'
+    use { 'anuvyklack/pretty-fold.nvim',
+      config = function()
+        require('core.plug.fold')
+      end,
+      requires = {
+        'anuvyklack/fold-preview.nvim',
+        'anuvyklack/keymap-amend.nvim'
+      }
+    }
+    use { 'norcalli/nvim-colorizer.lua',
+      config = function()
+        require('core.plug.colorizer')
+      end
+    }
+    -- Preview
+    use({ "iamcco/markdown-preview.nvim",
+      run = "cd app && npm install",
+      setup = function()
+        vim.g.mkdp_filetypes = { "markdown" }
+      end,
+      ft = { "markdown" },
+    })
+    use {
+      'kyazdani42/nvim-tree.lua',
+      requires = {
+        'kyazdani42/nvim-web-devicons',
+      },
+      tag = 'nightly',
+      config = function()
+        require('core.plug.nvim-tree')
+      end,
+    }
+    -- GitHub
+    use { 'lewis6991/gitsigns.nvim',
+      config = function()
+        require('core.plug.gitsigns')
+      end
+    }
+    -- Status Line and Tabs
+    use {
+      'nvim-lualine/lualine.nvim',
+      requires = {
+        { 'kyazdani42/nvim-web-devicons', opt = true },
+        { 'romgrk/barbar.nvim',
+          config = function()
+            require("core.plug.barbar")
+          end,
+        },
+      },
+      config = function()
+        require('core.plug.lualine')
+      end,
+    }
+    -- use {
+    --   'windwp/nvim-autopairs',
+    --   config = function()
+    --     require('core.plug.autopairs')
+    --   end,
+    -- }
+    -- Highlight
+    use {
+      'nvim-treesitter/nvim-treesitter',
+      requires = { 'p00f/nvim-ts-rainbow' },
+      run = ':TSUpdate',
+      config = function()
+        require('core.plug.treesitter')
+      end,
+    }
+    -- Autocomplete and lsp {{{
+    use({
+      "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+      as = "lsp_lines",
+      config = function()
+        require("lsp_lines").setup()
+      end,
+    })
+    use {
+      'hrsh7th/nvim-cmp',
+      config = function()
+        require("core.plug.cmp")
+      end,
+    }
+    use 'onsails/lspkind-nvim'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-nvim-lua'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'saadparwaiz1/cmp_luasnip'
+    -- use "rafamadriz/friendly-snippets"
+    use 'L3MON4D3/LuaSnip'
+    use 'jose-elias-alvarez/null-ls.nvim'
+    -- lsp
+    use 'neovim/nvim-lspconfig'
+    use 'williamboman/nvim-lsp-installer'
+    -- }}}
+  end,
+  config = {
+      compile_path = vim.fn.stdpath('config') .. '/prueba/packer_compiled.lua',
+    display = {
+      open_fn = require('packer.util').float,
     },
-    config = function()
-      require('core.plug.lualine')
-    end,
-  }
-  -- use {
-  --   'windwp/nvim-autopairs',
-  --   config = function()
-  --     require('core.plug.autopairs')
-  --   end,
-  -- }
-  -- Highlight
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    requires = { 'p00f/nvim-ts-rainbow' },
-    run = ':TSUpdate',
-    config = function()
-      require('core.plug.treesitter')
-    end,
-  }
-  -- Autocomplete and lsp {{{
-  use({
-    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-    as = "lsp_lines",
-    config = function()
-      require("lsp_lines").setup()
-    end,
-  })
-  use {
-    'hrsh7th/nvim-cmp',
-    config = function()
-      require("core.plug.cmp")
-    end,
-  }
-  use 'onsails/lspkind-nvim'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-nvim-lua'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'saadparwaiz1/cmp_luasnip'
-  -- use "rafamadriz/friendly-snippets"
-  use 'L3MON4D3/LuaSnip'
-  use 'jose-elias-alvarez/null-ls.nvim'
-  -- lsp
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/nvim-lsp-installer'
-  -- }}}
-end)
+  },
+}
