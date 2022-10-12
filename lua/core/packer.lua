494d8bbb7d3e0b1f81970fa8bac4cf001335f03f
TroubleC0re <fierrodylan34@gmail.com>
2022-10-12 00:49:40 -0600

    Update

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -1,3 +1,8 @@
+local packerPath = vim.fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
+if vim.fn.empty(vim.fn.glob(packerPath)) == 1 then
+  vim.fn.system("git clone --depth 1 https://github.com/wbthomason/packer.nvim " .. packerPath)
+end
+
 vim.cmd [[packadd packer.nvim]]
 vim.cmd([[
   augroup packer_user_config
