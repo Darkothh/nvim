6d18054057b54620c33900f574f04bcabe3c19cd
dark <dark@dark.com>
2024-02-12 15:44:54 -0600

    Add notes configs to markdown files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,3 +3,15 @@
 -- Add any additional autocmds here
 --
 vim.cmd("hi! LineNr guibg=none ctermbg=none")
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = augroup("markdown_conceal"),
+  pattern = { "markdown", ".md" },
+  callback = function()
+    vim.opt_local.conceallevel = 2
+  end,
+})
